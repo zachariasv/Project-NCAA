eec1f98b9d75e5b2c499c55f12259991fd2641ec
Zacharias Veiksaar <zacharias@veiksaar.se>
2024-12-15 21:04:46 +0100

    Updated date handling Updated script to ensure date handling was consistent with rest of code and worked for all cases

--- Match Results/fetch_match_results.py
+++ Match Results/fetch_match_results.py
@@ -25,21 +25,21 @@ def initialize_data():
     """
     if not os.path.exists(WEBSCRAPE_MATCHES_FILE):
         logging.info("No existing database found, creating new database...")
-        date = pd.to_datetime("1986-1-2").date() # Initialize with first date of data if no data is available
+        date = pd.to_datetime("1986-1-2") # Initialize with first date of data if no data is available
         columns = ["date", "home_team", "home_team_ranking", "home_team_score", "away_team", "away_team_ranking", "away_team_score", "gender"]
         df = pd.DataFrame(columns=columns)
         df.to_parquet(WEBSCRAPE_MATCHES_FILE)
     else:
         logging.info("Fetching data from existing database...")
         df = pd.read_parquet(WEBSCRAPE_MATCHES_FILE) # Load already fetched database
-        date = pd.to_datetime(df.date.max()).date()
+        date = pd.to_datetime(df.date.max())
     return df, date
 
 def setup_webdriver(date):
     """
     Set up the Chrome webdriver with the necessary options.
     """
-    logging.info(f"Setting up webdriver for {date}...")
+    logging.info(f"Setting up webdriver for {date.date()}...")
     options = webdriver.ChromeOptions()
     options.add_argument("--start-maximized")
     options.add_argument("--no-sandbox")
@@ -59,13 +59,13 @@ def scrape_data(df, browser, date):
     finished_scraping = False # Set up while loop
     
     while not finished_scraping:
-        date = (date + timedelta(days = 1)) # Fetch next date
+        date = pd.to_datetime(date + timedelta(days = 1)) # Fetch next date
 
         if date.month == 5 and date.day == 1: date = date.replace(month=11, day=1) # If the date is May 1st, jump to November 1st of the same year
         
-        if date > datetime.now().date(): # If the date is in the future, quit the script
+        if date > datetime.now(): # If the date is in the future, quit the script
             df.drop_duplicates().reset_index(drop = True).to_parquet(WEBSCRAPE_MATCHES_FILE) # Save data to parquet file once finished
-            date = pd.to_datetime(df.date.max()).date()
+            date = pd.to_datetime(df.date.max())
 
             finished_scraping = True 
             browser.quit()
@@ -96,13 +96,13 @@ def scrape_data(df, browser, date):
 
                 except Exception as e: # Simple error handling, often parsing of integers fails due to empty strings when elements are found but are empty on days with no matches. This is a crude but working fix.
                     pass
-            logging.info(f"Finished collecting data for {date}!")
+            logging.info(f"Finished collecting data for {date.date()}!")
         else:
-            logging.info(f"No matches found for {date}.")
+            logging.info(f"No matches found for {date.date()}.")
 
         if date.day in [1, 11, 21]: # Periodically save data
             df.drop_duplicates().reset_index(drop = True).to_parquet(WEBSCRAPE_MATCHES_FILE) # Save data to parquet file once finished
-            logging.info(f"Saved data on {date}.")
+            logging.info(f"Saved data on {date.date()}.")
             
     return df
 
@@ -112,8 +112,8 @@ def main():
     df = scrape_data(df, browser, date) # Scrape data
 
     df.drop_duplicates().reset_index(drop = True).to_parquet(WEBSCRAPE_MATCHES_FILE) # Save data to parquet file once finished
-    date = pd.to_datetime(df.date.max()).date()
-    logging.info(f"Finished scraping and saved data on {date}.")
+    date = pd.to_datetime(df.date.max())
+    logging.info(f"Finished scraping and saved data on {date.date()}.")
 
 if __name__ == "__main__":
     main()
